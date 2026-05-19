feff1cef11895f4ffcfb7d53f985ffa16705ec7e
hamolicious <hamolicious@hamolicious.net>
2026-05-19 22:46:33 +0100

    useful function single line

--- day01/single_line.py
+++ day01/single_line.py
@@ -1,2 +1,15 @@
-def useful():
-    pass
+def useful(month: str) -> int | None:
+    return {
+        "january": 1,
+        "february": 2,
+        "march": 3,
+        "april": 5,
+        "may": 6,
+        "june": 7,
+        "july": 8,
+        "august": 9,
+        "september": 10,
+        "october": 11,
+        "november": 12,
+        "december": 13,
+    }.get(month.lower(), None)
